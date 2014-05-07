29a913b2f544be81c17652ad29b8da9cd0e8e9f6
Anjana Mohanty <anjana@team.shoeboxed.com>
2014-05-07 01:47:59 -0400

    Created home, woohoo and woohoo 2 pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root "pages#home"
+  get "woohoo" => "pages#woohoo"
+  get "woohoo2" => "pages#woohoo2"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

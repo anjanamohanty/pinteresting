e54c109f341ee7d4e7b82779ff71d1d543aa1acf
Anjana Mohanty <anjana@team.shoeboxed.com>
2014-05-13 11:59:54 -0400

    Trying to revert commits

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   def home
   end
-  def page1
+
+  def woohoo
+  end
+
+  def woohoo2
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root "pages#home"
   get "page1" => "pages#page1"
+  get "page2" => "pages#page2"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

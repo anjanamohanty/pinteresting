b6cb9acd8adcf00bb8412568a4c0a8d2281028ea
Anjana Mohanty <anjana@team.shoeboxed.com>
2014-05-15 14:45:21 -0400

    At pinterest like brand - ready for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
   root "pages#home"
-  get "page1" => "pages#page1"
-  get "page2" => "pages#page2"
+  get "about" => "pages#about"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

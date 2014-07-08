a2ae283d14a8e08ce783fefa288ba9e10f00b683
Anjana Mohanty <anjana@team.shoeboxed.com>
2014-07-08 11:55:46 -0400

    JQuery Masonry changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-    #require for heroku / devise
-    #TODO - replace with actual host name
+  #require for heroku / devise
+  #TODO - replace with actual host name
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # Sets Paperclip to upload to Amazon S3
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
